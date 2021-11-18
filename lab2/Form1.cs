11782adfd92487255533cb97dd083653e7e7b7ba
nimraahmed <nimra1408@gmail.com>
2021-11-18 18:47:02 +0500

    Add project files.

---
+++ lab2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataFunctions dataObj = new lab2.DataFunctions();
+            string SQL_Slect = "Select * from uni";
+            dataGridView1.DataSource = dataObj.getData(SQL_Slect);
+
+        }
+    }
+}
